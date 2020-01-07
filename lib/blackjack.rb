5bc905b312334974d0f29f6a3fdec41b3480e811
Julianna Boyd <julianna.boyd91@gmail.com>
2020-01-07 01:47:28 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -34,7 +34,7 @@ def hit?(card_total)
   user_input = get_user_input
     if user_input == "h"
       card_total += deal_card
-  else if user_input == "s"
+  elsif user_input == "s"
       card_total 
   else 
   invalid_command
